01338253b6e2d2a0939b02c6b2b8f0717110e085
TorreJohnson <torre.johnson@gmail.com>
2018-03-30 11:29:40 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Todo extends Component {
-  render() {
-    return (
-      <li>
-        {this.props.text}
-      </li>
-    );
-  }
-};
+	handleClick = () => {
+		this.props.store.dispatch({
+			type: "DELETE_TODO",
+			id: this.props.id
+		});
+	};
+	render() {
+		return (
+			<li>
+				{this.props.text}
+				<button onClick={this.handleClick} />
+			</li>
+		);
+	}
+}
 
-export default Todo
+export default Todo;
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,14 +1,20 @@
 let id = 0;
 
-export default function manageTodo(state = {
-  todos: []
-}, action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      id++;
-      const todo = Object.assign({}, action.todo, { id: id });
-      return { todos: state.todos.concat(todo) };
-    default:
-      return state;
-  }
-};
+export default function manageTodo(
+	state = {
+		todos: []
+	},
+	action
+) {
+	switch (action.type) {
+		case "ADD_TODO":
+			id++;
+			const todo = Object.assign({}, action.todo, { id: id });
+			return { todos: state.todos.concat(todo) };
+		case "DELETE_TODO":
+			const filteredTodos = state.todos.filter(todo => todo.id !== action.id);
+			return { todos: filteredTodos };
+		default:
+			return state;
+	}
+}
